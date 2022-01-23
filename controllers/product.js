11584b6d0a0ec67576192f2665954afe18485729
IanMarcos <ian.marcos.ortiz@gmail.com>
2022-01-23 13:17:02 -0500

    Las imagenes se suben a imgBB

--- controllers/product.js
+++ controllers/product.js
@@ -1,11 +1,16 @@
+const axios = require('axios');
+const FormData = require('form-data');
+const fs = require('fs');
 const lookup = require('country-code-lookup')
 const Product = require('../models/product');
-const { processImgs } = require('../helpers/img-processing');
+const { getFormat, processImgs } = require('../helpers/img-processing');
 
 const createProduct = async(req, res) => {
     let { name, description='', price, discount=0, country } = req.body;
+    const files = req.files;
+
     price = Number(price);
-    discount = (Number(discount));
+    discount = Number(discount);
     country = country.toUpperCase();
 
     if(!name || name.length === 0){
@@ -31,9 +36,44 @@ const createProduct = async(req, res) => {
     }
 
     // //Validación de imagenes
-    // processImgs(imgs);
+    const validFormats = ['jpeg', 'jpg', 'png', 'webp'];
+    files.forEach(file => {
+        if(!validFormats.includes( getFormat(file.mimetype) )){
+            return res.status(400).json({err: 'Formato inadecuado de archivos'});
+        }
+    });
 
-    res.status(200).json( {results: req.body});
+    //Procesamiento de imagenes en caso de que alguna pese más de 1MB
+    const proccessedImgData = await processImgs(files);
+    //Si algun elemento es null hubo algún fallo procesando las imagenes
+    if (proccessedImgData.includes(null)){
+        return res.status(500).json({ err: 'Error inseperado procesando las imagenes' });
+    }
+
+    //Se verifica que ninguna imagen haya quedado de más de !MB
+    proccessedImgData.forEach(data => {
+        if(data.size >= 1000000){
+            return res.status(400).json({ err: 'Alguna imagen es demasiado grande' });
+        }
+    });
+
+    //Sea almacenan las imagenes en imgBB
+    //Array de promises
+    const promises = files.map( file => {
+        const url = "https://api.imgbb.com/1/upload?key=582dabb88b61248f8c2ff52fd47ede95"
+        const data = new FormData();
+        data.append('image',fs.createReadStream(file.path));
+        return axios.post(url, data, {
+            headers: data.getHeaders()
+        })
+        
+    })
+    
+    const responses = await Promise.all(promises);
+    // console.log(responses);
+    // responses.forEach(response => {})
+
+    res.status(200).json( {results: 'Hi'});
 
 }
 
--- helpers/img-processing.js
+++ helpers/img-processing.js
@@ -1,61 +1,84 @@
-const { Buffer } = require('buffer');
 const sharp = require('sharp');
+sharp.cache(false);
 
-function compressImg( imgData, format='jpeg' ) {
-    return new Promise( (resolve, reject ) => {
-        //solo png tiene la propiedad 'compressionLevel', todos los demas formatos tienen quality'
-        const compression = format === 'png'? { compressionLevel: 9 } : { quality: 50 };
+async function compressImg( imgPath, format='jpeg') {
+    //solo png tiene la propiedad 'compressionLevel', todos los demas formatos tienen quality'
+    const compression = format === 'png'? { compressionLevel: 9 } : { quality: 1 };
 
-        sharp(imgData)
-            .toFormat(format, compression)
-            .withMetadata()
-            .toBuffer()
-            .then( data => resolve(data))
-            .catch(err => reject(null));
-    });
-}
+    try {
+        //Pasar la imagen a Buffer y luego comprimir la imagen permite sobreescribir el archivo original sin riesgo
+        const buffer = await sharp(imgPath)
+        .withMetadata()
+        .toBuffer()
 
-function determineFileFormat(char){
-    switch (char) {
-        case '/':
-            return('jpeg');
-        case 'i':
-            return('png');
-        case 'U':
-            return('webp');
-        default:
-            return(undefined);
+        const info = await sharp(buffer)
+        .toFormat(format, compression)
+        .withMetadata()
+        .toFile(imgPath)
+
+        return info;
+        
+    } catch (error) {
+        return null;
     }
 }
 
-async function processImg(img) {
-    let imgString = img;
+function getFormat(mimetype){
+    return mimetype.slice(mimetype.indexOf('/')+1);
+}
 
-    if(imgString[0] === 'd'){
-        //Si el primer char es una d hay que remover el data uri
-        imgString = img.slice(img.indexOf(',')+1);
-    }
-    
-    const imgData = Buffer.from(imgString, 'base64');
 
-    if(imgData.length >= 1000000){
+async function processImgs(imgs) {
+    const newImgs = imgs.map( async(img) => {
+
+        const { mimetype, originalname, path, size } = img
+        if(size <= 1000000){
+            return img;
+        }
+
         //Se determina el formato del archivo
-        const format = determineFileFormat(imgString[0])
-        if(!format) reject('Formato de imagen inválido')
+        const format = getFormat(mimetype);
 
         try {
-            const compressedImg = await compressImg(imgData, format);
-            console.log(compressedImg.length);
+            const compressedImg = await compressImg(path, format, originalname);
+            if(compressedImg.size <= 1000000){
+                return compressedImg;
+            }
+
+            const { width, height } = compressedImg;
+            return await resizeImg(path, format, width/2, height/2);
             
         } catch (error) {
-            console.log('Error en la compresión');
+            console.log(error);
+            return null;
         }
+    });
+    
+    return newImgs;
+}
+
+async function resizeImg( imgPath, format, w, h ) {
+    const compression = format === 'png'? { compressionLevel: 9 } : { quality: 1 };
+    try {
+        const buffer = await sharp(imgPath)
+        .resize(w, h)
+        .withMetadata()
+        .toBuffer()
+
+        const info = await sharp(buffer)
+        .toFormat(format, compression)
+        .withMetadata()
+        .toFile(imgPath)
+
+        return info;
         
-
-
+    } catch (error) {
+        console.log(error);
+        return null;
     }
 }
     
 module.exports = {
-    processImg
+    getFormat,
+    processImgs
 };
