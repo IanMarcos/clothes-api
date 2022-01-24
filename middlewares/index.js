2d5441a730116b66c52691aaecb1608bd8312a81
IanMarcos <ian.marcos.ortiz@gmail.com>
2022-01-23 21:55:09 -0500

    Codigo basura eliminado

--- middlewares/index.js
+++
@@ -1,7 +0,0 @@
-const { ...fieldValidator } = require('./fields-validator');
-const { ...dbValidator } = require('./db-validator');
-
-module.exports = {
-    ...fieldValidator,
-    ...dbValidator,
-};
